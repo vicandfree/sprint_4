52c6179b6c9e31a1698b9e2134b2034c2a0f785f
vicandfree <70838661+vicandfree@users.noreply.github.com>
2023-09-03 14:05:47 +0300

    Delete src/test/java/SamokatFirefoxTest.java

--- src/test/java/SamokatFirefoxTest.java
+++
@@ -1,9 +0,0 @@
-import org.openqa.selenium.firefox.FirefoxDriver;
-
-public class SamokatFirefoxTest extends SamokatTest{
-    @Override
-    public void initDriver() {
-        driver = new FirefoxDriver();
-        driver.get("https://qa-scooter.praktikum-services.ru/");
-    }
-}
