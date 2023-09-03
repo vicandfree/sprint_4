34b57c90d0e77b4a25b976769e0891ef98bc6b7e
vicandfree <70838661+vicandfree@users.noreply.github.com>
2023-09-03 20:16:30 +0300

    Update SamokatHomePageTest.java

--- src/test/java/SamokatHomePageTest.java
+++ src/test/java/SamokatHomePageTest.java
@@ -60,8 +60,8 @@ public class SamokatHomePageTest {
         home.navigateToHomePage();
         home.waitUntilLoaded();
         home.acceptCookies();
-
-        assertEquals(home.getAnswerToFAQ(index), answer);
+        
+        assertEquals(answer, home.getAnswerToFAQ(index));
     }
 
     @After
